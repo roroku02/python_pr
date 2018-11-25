255ca8d18be64430bfcb34d5a386e36547607e44
roroku02 <roroku02@gmail.com>
2018-11-25 17:12:21 +0900

    リスト内包表記・文字列の結合

--- list.py
+++ list.py
@@ -1,17 +1,11 @@
 import random
 
-my_list = []
+rand_num = [random.randint(0, 100) for i in range(20)]
+print([x**2 for x in rand_num])
 
-for i in range(20):
-    my_list.append(random.randint(0, 200))
+text_array = ['conputer', 'tv', 'work', 'desk']
+text_comma = ','.join(text_array)
+print(text_comma)
 
-print('normal_list::')
-print(my_list)
-
-my_list = map(lambda x: x * x, my_list)
-print('square_list::')
-print(list(my_list))
-
-my_list = list(filter(lambda x: x <= 2000, my_list))
-print('filter 2000::')
-print(list(my_list))
+text_normal = text_comma.split(',')
+print(text_normal)
